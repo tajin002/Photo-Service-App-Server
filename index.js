c5bbae673bf87ea82c8b0ce7e1547fd5b4cd3dbb
tajin002 <tajinshawon002@gmail.com>
2022-11-07 22:11:00 +0600

    server side created with middle ware

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+//middle ware
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/' , (req,res)=>{
+    res.send('server is running');
+});
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+    
+})
